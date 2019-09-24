e98588f8b02ad65500740bd6dd1757b1a2e335c2
Caleb Shepard <cshepard@Calebs-MacBook-Air.local>
2019-09-23 23:09:08 -0400

    Fix dependencies, add run.sh

---
+++ kernel/syscall.d
@@ -0,0 +1,4 @@
+kernel/syscall.d kernel/syscall.o: kernel/syscall.c \
+ kernel/../include/types.h kernel/defs.h kernel/../include/param.h \
+ kernel/mmu.h kernel/proc.h kernel/../include/x86.h \
+ kernel/../include/syscall.h kernel/sysfunc.h
